e1a10171ebe0c1cec926de26ab757a3f48098efe
Cadu Zanin <caduzanin123@gmail.com>
2025-06-11 11:03:23 -0300

    README.md corrigido

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import speech_recognition as sr
 
 pygame.init()
 inicializarBancoDeDados()
-teste = 0
+#isso daqui é de propósito que esta aqui
 tamanho = (1000, 700)
 tamanho_boasvindas = (100, 100)
 relogio = pygame.time.Clock()
